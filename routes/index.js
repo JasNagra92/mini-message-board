5c634fa58963d9b540ae0fbad746b862fce0f4f7
newbiejas <jaskarannagra@homail.com>
2022-08-20 20:46:06 -0700

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+const messages = [
+  {
+    text: "Hi There!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World",
+    user: "moongli",
+    added: new Date()
+  }
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages:messages });
 });
+
+router.get('/new', (req,res,next)=>{
+  res.render('form', {title: 'test'})
+})
+
+router.post('/new', (req,res,next)=>{
+  messages.push({text: req.body.text, user: req.body.author, added: new Date()});
+  res.redirect('/')
+})
 
 module.exports = router;
